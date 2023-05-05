f36712dd94013f2a7f63e7b5f2301f155bea963e
stylessh <stylesshjs@gmail.com>
2023-05-05 12:58:50 -0400

    feat: gift modal redesigned

--- src/components/GiftModal.tsx
+++ src/components/GiftModal.tsx
@@ -1,3 +1,4 @@
+import { Popover } from "@headlessui/react";
 import { GiftIcon } from "@heroicons/react/24/outline";
 
 type Props = {
@@ -7,26 +8,34 @@ type Props = {
 
 const GiftModal = ({ gifts, buyGift }: Props) => {
   return (
-    <article className="absolute max-w-xs px-4 py-2 text-white rounded-md bg-main right-5 bottom-20">
-      {/* icon */}
-      <header className="inline-flex items-center space-x-2">
-        <GiftIcon className="w-5" />
+    <Popover className="absolute right-5 bottom-20">
+      <Popover.Panel
+        as="div"
+        className="absolute right-0 w-64 max-w-xs px-4 py-2 mb-4 text-white rounded-md shadow-xl md:w-72 bottom-full bg-main"
+      >
+        {/* icon */}
+        <header className="inline-flex items-center space-x-2">
+          <GiftIcon className="w-5" />
+          <h2 className="font-semibold">Do you want to send a gift?</h2>
+        </header>
 
-        <h2 className="font-semibold">Do you want to send a gift?</h2>
-      </header>
+        <ul className="my-2 space-y-2">
+          {gifts.map((gift) => (
+            <li key={gift.id}>
+              <button className="hover:underline" onClick={() => buyGift(gift)}>
+                <h3 className="text-sm text-left">
+                  {gift.name} - ${gift.price / 1000}
+                </h3>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </Popover.Panel>
 
-      <ul className="space-y-2">
-        {gifts.map((gift) => (
-          <li key={gift.id}>
-            <button className="hover:underline" onClick={() => buyGift(gift)}>
-              <h3>
-                {gift.name} - ${gift.price / 1000}
-              </h3>
-            </button>
-          </li>
-        ))}
-      </ul>
-    </article>
+      <Popover.Button className="p-4 rounded-full bg-main">
+        <GiftIcon className="w-5 h-5 text-white" />
+      </Popover.Button>
+    </Popover>
   );
 };
 
